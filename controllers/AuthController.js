39a6f949f8247734b56956f6038d5cc1c8012d71
shashank das vaishnav <141327449+shashankdas123@users.noreply.github.com>
2024-09-10 14:18:24 +0530

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -43,7 +43,7 @@ module.exports.Login = async (req, res, next) => {
          withCredentials: true,
          httpOnly: false,
        });
-       res.status(201).json({ message: "User logged in successfully", success: true });
+       return res.status(201).json({ message: "User logged in successfully", success: true });
        next()
     } catch (error) {
       console.error(error);
